74831cdae2f468c778a746bfc8f5dd7431204b5a
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-04-22 23:34:46 +0100

    Completed get User, and the authmiddleware for get user

--- middleware/authMiddlewear.js
+++ middleware/authMiddlewear.js
@@ -2,13 +2,41 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   try {
-  } catch (error) {
-    console.log("error in authMiddlewear", error);
-    res
-      .status(500)
-      .json({
+    // Ensure req.body is defined
+    req.body = req.body || {};
+    // Get token from header
+    const authHeader = req.headers["authorization"];
+
+    // Check if token exists
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
         success: false,
-        message: "Internal Server Error or test failure!",
+        message: "Token not found or malformed",
       });
+    }
+
+    // Extract token
+    const token = authHeader.split(" ")[1];
+
+    // Verify token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized User!",
+        });
+      } else {
+        // Attach the decoded user to the request object and call next()
+        req.body.id = decoded.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log("Error in authMiddleware:", error);
+    res.status(500).json({
+      success: false,
+      message: "Provide Auth token in the header",
+      error: error.message,
+    });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { getUserController } = require("../controllers/userController");
+const authMiddlewear = require("../middleware/authMiddlewear");
 
 const router = express.Router();
 
 // ROUTES
 
 // GET USER
-router.route("/getUser").get(getUserController);
-
+router.route("/getUser").get(authMiddlewear, getUserController);
 
 module.exports = router;
