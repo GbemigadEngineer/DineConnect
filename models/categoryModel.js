6b6e7a5debcbc99c893424a85db3b678ab82faf3
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-04-29 18:58:34 +0100

    Started working on the category resource

---
+++ models/categoryModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+// schema
+const categorySchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Category title is required!"],
+    },
+    imageUrl: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dqj0xv4gk/image/upload/v1698231232/food-app/food-category.png",
+      required: [true, "Category image is required!"],
+    },
+  },
+  { timestamps: true }
+);
+// create Model
+const Category = mongoose.model("Category", resturantSchema);
+
+// export
+module.exports = Category;
